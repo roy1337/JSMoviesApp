acb20f92ee80706fea2cc909f2e3e8a1be5be49d
Roy <40725093+roy1337@users.noreply.github.com>
2022-03-14 14:19:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=027792909d73cf14966305d916cda88c&page=1'
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=027792909d73cf14966305d916cda88c&query="'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=&query="'
 
 const form = document.getElementById('form')
 const search = document.getElementById('search')
